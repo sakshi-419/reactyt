68864cce3c3fe81e4fc4b661030ba031da072e75
sakshi-419 <witness.code04@gmail.com>
2026-02-16 14:31:32 +0530

    switched to hashrouter

--- TaskBoard/src/App.jsx
+++ TaskBoard/src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Board from "./pages/Board";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/board" element={<Board />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
